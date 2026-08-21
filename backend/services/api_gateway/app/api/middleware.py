e7423e34202a56192ba9e5e7510882d4881597d2
mrakolice <postevoy.svyatoslav@gmail.com>
2026-08-21 15:46:43 +0300

    run middleware

--- backend/services/api_gateway/app/api/middleware.py
+++ backend/services/api_gateway/app/api/middleware.py
@@ -6,6 +6,7 @@ logger = structlog.get_logger(__name__)
 
 class GatewayMiddleware(BaseHTTPMiddleware):
     async def dispatch(self, request, call_next):
+        logger.info('dispatch')
         logger.info(f'{request.method} {request.url} {request.headers}')
 
         response = await call_next(request)
